cff17b09e4dba2f08a24eb5d188fe347a80d2543
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-02-12 21:03:44 +0545

    userdisplay

--- frontend/frontend1/src/App.jsx
+++ frontend/frontend1/src/App.jsx
@@ -14,6 +14,7 @@ import DocumentUploadPage from "./UserPages/DocumentUploadPage";
 import DisplayAllDocument from "./pages/DisplayAllDocument";
 import DocumentUpdatePopup from "./components/DocumentUpdatePopup";
 import Navbar from "./UserPages/Navbar";
+import UserDisplayAllDocument from "./UserPages/UserDisplayAllDocument";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
         <Route path="/displayalldocs" element={<DisplayAllDocument />} />
         <Route path="/popup" element={<DocumentUpdatePopup />} />
         <Route path="/nav" element={<Navbar />} />
+        <Route path="/user-docs" element={<UserDisplayAllDocument />} />
       </Routes>
     </>
   );
